a2a19696eb2c19b492624f662f50f996acaa0a9a
Irfan Ramzan Parray <parrayirfan9@gmail.com>
2026-05-24 07:13:53 +0000

    added a flask project

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
-print(app)
+@app.route("/")
+def home():
+  return "hello world how are you"
+
+if __name__ == "__main__":
+  app.run(host = '0.0.0.0', debug=True)
+  #print("inside if")
